b7febf704c7aff238649bf0b3a604eaefc1333ae
JouRJNey <nievjosh9@gmail.com>
2020-06-16 17:26:15 +0200

    Websites: removed Test subsection

--- ds-script.js
+++ ds-script.js
@@ -182,29 +182,6 @@ jQuery(document).ready(function ($) {
   });
 });
 
-//===================Test=======================//
-//===================================================//
-/* @Note: not sure e.pageX will work in IE8 */
-(function (window) {
-  /* A full compatability script from MDN: */
-  var supportPageOffset = window.pageXOffset !== undefined;
-  var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
-
-  /* Set up some variables  */
-  var demoItem3 = document.getElementById("demoItem3");
-  /* Add an event to the window.onscroll event */
-  window.addEventListener("scroll", function (e) {
-    /* A full compatability script from MDN for gathering the x and y values of scroll: */
-    var y = supportPageOffset
-      ? window.pageYOffset
-      : isCSS1Compat
-      ? document.documentElement.scrollTop
-      : document.body.scrollTop;
-
-    demoItem3.style.top = -y + 50 + "px";
-  });
-})(window);
-
 /*
  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄                 ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄
 ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌               ▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
